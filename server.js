6d4d9b0c69e2fb5705c296b92de8fa09c56e7e9a
Daniel J Lee <danieljoonlee@gmail.com>
2015-08-27 17:48:56 -0700

    index and server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 var express = require('express');
 var server = express();
 
-//server.use(express.static('public'));
+server.use(express.static('public'));
+server.set('view engine', 'ejs');
+
+server.get('/poll', function(req,res) {
+	res.render('poll',{name:'daniel'});
+});
 
 server.listen(3000);
+
